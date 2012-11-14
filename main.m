66de0b074ff61f08e201d32dfd5101210b04976b
Sidney Just <Sidney.just@gmail.com>
2012-11-14 14:11:17 +0100

    Command line arguments are now read correctly and support Extended Brainfuck Type II

--- main.m
+++ main.m
@@ -33,9 +33,13 @@ int main(int argc, const char *argv[])
         {
             for(int i=1; i<argc; i++)
             {
-                if(strcmp(argv[i], "-x"))
+                if(strcmp(argv[i], "-x") == 0)
                 {
                     type = kBFExtendedInterpreterDialectTypeI;
+                }
+                else if(strcmp(argv[i], "-x2") == 0)
+                {
+                    type = kBFExtendedInterpreterDialectTypeII;
                 }
                 else
                 {
